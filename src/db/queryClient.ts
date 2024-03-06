45ba655bcf7884e096190ad550b97b548c4403e8
Roman Njoroge <roman.njoroge@njuguna.com>
2024-03-07 01:45:36 +0300

    feat: Defining db that i can query

---
+++ src/db/queryClient.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+const postgres_user = process.env.PG_USER;
+const password = process.env.PG_PASSWORD;
+const host = process.env.PG_HOST;
+const port = process.env.PG_PORT;
+const database = process.env.PG_DATABASE;
+
+const queryClient = postgres(`postgres://${postgres_user}:${password}@${host}:${port}/${database}`, { max: 1});
+const db = drizzle(queryClient);
+
+export default db;
