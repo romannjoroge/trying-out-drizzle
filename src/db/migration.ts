5d2bf2099fafeb2be5867c56fb6c52a177ff3da9
Roman Njoroge <roman.njoroge@njuguna.com>
2024-03-07 01:42:43 +0300

    feat: Defining migration file

---
+++ src/db/migration.ts
@@ -0,0 +1,26 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const postgres_user = process.env.PG_USER;
+const password = process.env.PG_PASSWORD;
+const host = process.env.PG_HOST;
+const port = process.env.PG_PORT;
+const database = process.env.PG_DATABASE;
+
+const migrationClient = postgres(`postgres://${postgres_user}:${password}@${host}:${port}/${database}`, { max: 1});
+
+async function main() {
+    try {
+        migrate(drizzle(migrationClient), {
+            migrationsFolder: "src/db/migrations"
+        })
+    } catch(err) {
+        console.log("Could Not Migrate");
+        console.log(err);
+    }
+}
+
+main();
