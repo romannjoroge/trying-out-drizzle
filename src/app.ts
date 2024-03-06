1306b2a5aa43625ebc68c97646d12c74d0eb197b
Roman Njoroge <roman.njoroge@njuguna.com>
2024-03-07 02:26:16 +0300

    defining test routes

---
+++ src/app.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+const app = express();
+import db from './db/queryClient.js';
+import { classStudents, classes, students } from './db/schema.js';
+import { eq } from 'drizzle-orm';
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+    const data = await db.select({
+        name: students.name,
+        dob: students.dob
+    }).from(students);
+
+    return res.json(data);
+})
+
+app.post('/users', async (req, res) => {
+    let {
+        name,
+        dob
+    } = req.body();
+
+    await db.insert(students).values({
+        name,
+        dob
+    });
+
+    return res.status(201).json({message: "Student Created"});
+})
+
+app.post('/class', async (req, res) => {
+    let {
+        name
+    } = req.body;
+
+    await db.insert(classes).values({
+        name
+    });
+
+    return res.status(201).json({message: "Class Created"});
+});
+
+app.get('/studentsClass', async (req, res) => {
+    const data = await db.select({
+        student: students.name,
+        class: classes.name
+    }).from(students)
+    .leftJoin(classStudents, eq(classStudents.studentId, students.id))
+    .leftJoin(classes, eq(classes.id, classStudents.classId));
+
+    return res.json(data);
+})
+
+
+app.get('*', (req, res) => {
+    return res.status(500).json({message: "Route Does Not Exist"})
+});
+
+app.listen(6500, () => {
+    console.log("Server is listening on port 6000...")
+})
